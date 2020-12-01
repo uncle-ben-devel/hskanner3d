d7e2b0c2c1b3524915eceb990fb30807e0e7b119
Benedikt Reinberger <rebe1011@hs-karlsruhe.de>
2020-12-01 15:13:45 +0100

    Changed the bootloader flash script for easier use when flashing many Pies.

--- installation_setup/bootloader-flash-utils/flash_bootloader.sh
+++ installation_setup/bootloader-flash-utils/flash_bootloader.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-if [ "$USER" = "root" ]
-then
-	# update firmware for better power performance
-	if [[ $(ping -c 1 -q google.com >&/dev/null; echo $?) == "0" ]]
-	then
-		apt full-upgrade	# updates the firmware on the pi
-	else
-		echo "This system is not connected to the internet. Firmware could not be updated."
-	fi
-	# flash pxe-bootloader. if you want to know how to make your own bootloader with the settings you want, see https://www.raspberrypi.org/documentation/hardware/raspberrypi/bcm2711_bootloader_config.md
-	rpi-eeprom-update -d -f ./pieeprom-new.bin
-	echo "This system will reboot now."
-	reboot
+if test -f /home/pi/updated-flag; then
+    echo "Bootloader was updated already, shutting down..." | tee -a bootloader_flash.log
+    rm /home/pi/updated-flag
+    sudo shutdown now
+    exit
 else
-	echo "Please execute the script as root or with the sudo command and ensure you have a working internet connection."
+    touch updated-flag
 fi
+
+# update firmware for better power performance
+if [[ $(ping -c 1 -q google.com >&/dev/null; echo $?) == "0" ]]
+then
+	sudo apt full-upgrade	# updates the firmware on the pi
+else
+	echo "This system is not connected to the internet. Firmware could not be updated." | tee -a bootloader_flash.log
+fi
+
+# flash pxe-bootloader. if you want to know how to make your own bootloader with the settings you want, see https://www.raspberrypi.org/documentation/hardware/raspberrypi/bcm2711_bootloader_config.md
+sudo rpi-eeprom-update -d -f /home/pi/./pieeprom-new.bin | tee -a bootloader_flash.log
+echo "This system will reboot now." | tee -a bootloader_flash.log
+sudo reboot
